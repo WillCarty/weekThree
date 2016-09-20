a1f9869f4e178cdd5fdf89036856c9b58e86089d
William Carty <will.carty@interapthq.com>
2016-09-20 09:41:21 -0400

    jqueryChallege

---
+++ dayTwo/selectorChallenge.js
@@ -0,0 +1,12 @@
+
+function headerColor(){
+$("h3").css("color","red");
+
+}
+
+function listOne(){
+    $("#list1").css("color", "blue");
+}
+function hidePara(){
+        $("p").fadeOut("slow","linear");
+    }
